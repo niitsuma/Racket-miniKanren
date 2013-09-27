292c87ae55c42dd6b4523fd48bad86c9dab652ef
Hirotaka Niitsuma <niitsuma@mub.biglobe.ne.jp>
2013-09-27 18:47:48 +0900

    mod comment2

--- matchee.scm
+++ matchee.scm
@@ -4,9 +4,6 @@
  matchee)
 
 
-  ;(import (rnrs) (cKanren ck))
-
-;(require "mk.scm")
 (require "mk.rkt")
 (require "miniKanren.scm")
 (require "matche.scm")
@@ -14,22 +11,10 @@
 
 
 
-;; (define-syntax-computation syntax-unquote-pat-extract
-;;   (computation-rules (unquote)
-;;     ((_ () ) (syntax-return ()))    
-;;     ((_ (unquote e )) (syntax-return (e)))
-;;     ((_ (h . t) ) (syntax-do 
-;;     		   (h1 <- (syntax-unquote-pat-extract h))
-;;     		   (t1 <- (syntax-unquote-pat-extract t))
-;;     		   (ht1 <- (syntax-append h1 t1))
-;;     		   (syntax-return ht1)))
-;;     ((_ e )  (syntax-return ()))
-;; ))
 
 
 (define-syntax-computation syntax-macthe-pat-refine
   (computation-rules (___  unquote )
-    ;;((_ () ) (syntax-return ()))    
 
     ((_ ( (h ___  . t) . (body ... )  ) )
      (syntax-do
@@ -42,16 +27,12 @@
       (vs <- (syntax-extract-unique-unquoted h1))
       (vs2 <- (syntax-extract-unique-unquoted t2 vs))
 
-      ;(syntax-return  ( (h1 . t2) . b2 ) )
 
       (var <- (syntax-gensym))
       (var2 <- (syntax-gensym))
-      ;;(vs  <- (syntax-unquote-pat-extract h))
-      ;; (syntax-return ( (h . t) . (body ... ) ))
       (syntax-return	       
                (,var2 . (			
       			(fresh 
-      			 ;vs
       			 (var . vs2 )
       			 (appendo var `t2 var2)
       			 (for-eache (lambda (var . vs )  (== `h var)) var . vs ) 
@@ -63,23 +44,6 @@
       ))
 
 
-      ;; (hb1 <- (syntax-macthe-pat-refine
-      ;; 	       (,var2 . (			
-      ;; 			(fresh 
-      ;; 			 ;vs
-      ;; 			 (var . vs )
-      ;; 			 (for-eache (lambda (var . vs )  (== `h var)) var . vs ) 			 
-      ;; 			 . (body ...) 
-      ;; 			 )
-      ;; 			    )) ) )
-      ;; ;(syntax-return hb1 )
-      ;; (b1 <- (syntax-cdr hb1))
-      ;; (h1 <- (syntax-car hb1))
-      ;; (tb2 <- (syntax-macthe-pat-refine  (t . b1 ) ))
-      ;; (b2 <- (syntax-cdr tb2))
-      ;; (t2 <- (syntax-car tb2))
-      ;; (syntax-return  ( (h1 . t2) . b2 ) )
-      ;; ))
 
     ((_ ( (h . t)  . ( body ... ) ) ) 
      (syntax-do 
@@ -95,7 +59,7 @@
     ((_ e )   (syntax-return e))
 ))
 
-
+;; ;; example usage
 ;; (syntax-inspect (syntax-macthe-pat-refine  (,a  d1 d2 d3   )))
 ;; (syntax-inspect (syntax-macthe-pat-refine  ((,a ,b)  d1 d2 d3   )))
 ;; (syntax-inspect (syntax-macthe-pat-refine  ((,a ___ ,b)  d1 d2 d3   )))
@@ -122,63 +86,3 @@
 
 
 
-
-
-
-
-
-
-
-
-;; (match
-;;  '((1 (2 3)) (10 (2 30)) (100 (2 300))  1 2 3)
-;;  ;(`(,x . ,y)   (list x y))
-;;   ;(`( (,a (2 ,c)) ... . ,x)   (list x a c))
-;;   (`( (,a (2 ,c)) ... ,x ...)   (list x a c))
-;;  )
-
-
-;; (match
-;;  '(u ___  )
-;;  ;'(u ___  v)
-;;  (`(,p ,q . ,r)   (list p q r))
-;;   ;(`( (,a (2 ,c)) ... . ,x)   (list x a c))
-;;  ;(`( (,a (2 ,c)) ... ,x ...)   (list x a c))
-;;  )
-
-
-;; (run* 
-;;  (q)     
-;;  (fresh (x)
-;; 	(appendo '(1 2) `(,x) q)
-;; 	;(appendo '(1 2) '(3 4) q)
-;; 	))
-
- 
-
-;; (run* 
-;;  (q)     
-;;  (matche 
-;;   '((1 (2 3)) (10 (2 30)) (100 (2 300)))
-
-;;   (,g93793
-;;    (fresh
-;;     (a c)
-;;     (for-eache (lambda (g93793 a c) (== `(,a (2 ,c)) g93793)) g93793 a c)
-;;     (== q `(,a ,c))
-;;     ))))
-
-
-;; (run5
-;;  (q)     
-;;  (matche
-;;   '(1 2 3 4 5)
-;;   (,g92101
-;;    (fresh
-;;     (g92002 b a)
-;;     (appendo g92002 `(,b) g92101)
-;;     (for-eache (lambda (g92002 a) (== `,a g92002)) g92002 a)
-;;     (== q  `(,a ,b ,g92002 ,g92101 ))
-
-;;   ))
-;; ))
